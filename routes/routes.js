b9cc9e8c398dfab53c290f7bd7dc4e69563e3a91
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-04-15 13:51:47 -0400

    fixed unused variable

--- routes/routes.js
+++ routes/routes.js
@@ -1,30 +1,30 @@
 // Load the MySQL pool connection
 const pool = require('../data/config');
 
-const users = [{
-    id: 1,
-    name: "Instructor",
-    email: "instructor_345@stevens.edu",
-},
-{
-    id: 2,
-    name: "TA",
-    email: "ta_345@stevens.edu",
-},
+const users = [
+    {
+        id: 1,
+        name: 'Instructor',
+        email: 'instructor_345@stevens.edu',
+    },
+    {
+        id: 2,
+        name: 'TA',
+        email: 'ta_345@stevens.edu',
+    },
 ];
 
-const router = app => {
+const router = (app) => {
     app.get('/', (request, response) => {
         response.send({
-            message: 'Node.js and Express REST API'
+            message: 'Node.js and Express REST API',
         });
     });
 
     app.get('/users', (request, response) => {
         response.send(users);
-        
-    }); 
-    
+    });
+
     // Display all developers query from remote DB
     app.get('/handles', (request, response) => {
         pool.query('SELECT * FROM Handle', (error, result) => {
@@ -39,18 +39,17 @@ const router = app => {
         pool.query('SELECT * FROM Handle WHERE handle=?', handle, (error, result) => {
             if (error) throw error;
             response.send(result);
-        }); 
+        });
     });
     // Add a new developer
     app.post('/handles', (request, response) => {
         pool.query('INSERT INTO Handle SET ?', request.body, (error, result) => {
             if (error) throw error;
-
+            console.log(result);
             response.status(201).send(`Developer added`);
         });
     });
-
-}
+};
 
 // Export the router
 module.exports = router;
